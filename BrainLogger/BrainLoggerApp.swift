7db556c65aeb5c4bce3b4e98701c1ea26135cd7b
vkyuen <vkyuen@users.noreply.github.com>
2024-12-17 17:18:22 -0800

    Initial Commit

---
+++ BrainLogger/BrainLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainLoggerApp.swift
+//  BrainLogger
+//
+//  Created by Veronica Yuen on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
